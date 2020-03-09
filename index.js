211ebd6f8dd56760a3093ce90b2b63ce8a888776
kwak123 <kwak123@gmail.com>
2020-03-09 19:57:22 -0400

    refactor code inside export

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const child_process = require('child_process');
 
-let shouldClose = true;
+module.exports = absoluteFilePath => {
+  let shouldClose = true;
+  process = child_process.fork(absoluteFilePath);
 
-process.on('close', () => {
-  shouldClose = false;
-});
+  process.on('close', () => {
+    shouldClose = false;
+  });
 
-process.on('exit', () => {
-  shouldClose = false;
-});
+  process.on('exit', () => {
+    shouldClose = false;
+  });
 
-setTimeout(() => {
-  if (shouldClose) {
-    console.log('Process did not close, killing');
-    process.kill(process.pid);
-  }
-}, 1000);
+  setTimeout(() => {
+    if (shouldClose) {
+      const file = absoluteFilePath.split('/').pop();
+      console.log(
+        `Process for file ${file} did not close in 3 seconds, killing`,
+      );
+      process.kill(process.pid);
+    }
+  });
+};
