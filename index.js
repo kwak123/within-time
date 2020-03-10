9005c316659040c081de5f7095ea4bec823e0223
kwak123 <kwak123@gmail.com>
2020-03-10 11:50:08 -0400

    move cli code to lib

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 #!/usr/bin/env node
 
-const withinTime = require('./lib/withinTime');
-
-const [, , fileName] = process.argv;
-
-if (!fileName) {
-  return console.error('Filename must be provided');
-}
-
-withinTime(fileName);
+const cli = require('./lib/cli');
+cli(process.argv);
