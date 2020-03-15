8f1c295d969ba96d2f0a68337a972ecc6dedd097
kwak123 <kwak123@gmail.com>
2020-03-15 18:51:49 -0400

    changed fsHelper test to use fsScenarios, add test for getFileNames

--- test/fsHelper.spec.js
+++ test/fsHelper.spec.js
@@ -3,7 +3,7 @@ const FSHelper = require('../lib/fsHelper');
 
 describe('FSHelper', () => {
   describe('with directory', () => {
-    const directoryPath = path.resolve('./test/scenarios');
+    const directoryPath = path.resolve('./test/fsScenarios');
     let subject;
 
     beforeEach(() => {
@@ -19,6 +19,16 @@ describe('FSHelper', () => {
     describe('isFile', () => {
       it('should return false', () => {
         expect(subject.isFile).toBe(false);
+      });
+    });
+
+    describe('getFileNames', () => {
+      it('should return all file names', async () => {
+        const expectedFileNames = ['fileOne.js', 'fileTwo.js'];
+        const receivedFileNames = await subject.getFileNames();
+        expectedFileNames.forEach(fileName =>
+          expect(receivedFileNames).toContain(fileName),
+        );
       });
     });
   });
